fc18f7afeab941545ebec00b95577a054bff031b
ice <bingsogood@gmail.com>
2017-08-22 16:17:23 +0800

    init

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by ice on 2017/8/22.
+//  Copyright © 2017年 ICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by ice on 2017/8/22.
+//  Copyright © 2017年 ICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
